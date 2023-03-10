a6a3a33379481c768cba46d4dbcabc9788e33c6b
sarthakshah07 <sarthakshah44993@gmail.com>
2023-03-10 11:23:30 +0530

    issue resolved

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,36 +1,36 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import Marquee from "react-fast-marquee";
 import { useSelector } from "react-redux";
 import HomeCard from "../../components/HomeCard";
 import WrapperComponent from "../../components/WrapperComponent";
-import { counterSelector } from "../../slice/counterSlice";
+import { counterSelector } from "../../slice/counter/counterSlice";
 import './_home.css'
+import headcardicon from '../../assets/images/headcardicon.png'
 
 const HomeScreen = () => {
   const { cardsData } = useSelector(counterSelector);
-
+  const backstyle ={
+    backgroundColor:"black"
+  }
   return (
-    <WrapperComponent>
-      {/* <MyButton
-        fullWidth={true}
-        title="sign in"
-        variant="outlined"
-        handleClick={handleSubmit}
-      /> */}
-
-      {cardsData.map((cardItem) => (
-        <Grid
-          item
-          className="maingrid"
-          xs={12/9}
-          // sm={2}
-          // md={2}
-          // lg={2}
-          // xl={2}
+    <WrapperComponent sx={backstyle}>
+      <Marquee
+       speed={200} gradientWidth={200} gradientColor={[40,40,40]}><h1 style={{color:"green"}}><img src={headcardicon} alt=" "></img>Choose Card</h1></Marquee>
+      <Grid container
+        xs={12}
+       className="maingrid"
+       sx={{display:"flex",justifyContent:"center"}}
         >
-          <HomeCard data={cardItem} />
+      {cardsData.map((cardItem) => (
+        <Grid item
+        xs={12/27}
+        >
+        <HomeCard
+         data={cardItem} />
         </Grid>
       ))}
+      </Grid >
     </WrapperComponent>
   );
 };
